6830bd678cb394abce31ef0bee00034728c0939a
Ricardo <limaricardop@gmail.com>
2021-08-10 22:40:10 -0300

    App init - Components NavScreen

---
+++ components/TabIcon.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {
+    View,
+    Text,
+    Image
+} from 'react-native'
+
+import { FUNCTION_TYPES, COLORS } from '../constants'
+
+const TabIcon = () => {
+    return(
+        <View>
+            <Text>Tab</Text>
+        </View>
+    )
+}
+
+export default TabIcon;
